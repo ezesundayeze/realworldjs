654bcc323890b5f645e92a22ca3e8d3afe4d09db
Eze Sunday <rexeze@gmail.com>
2017-04-22 11:19:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e){ 
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignTo = document.getElementById('issueAssignToInput').value;
+    var issueId = chance.guid();
+    var issueStatus ='Open';
+    
+    var issue = {
+    id:issueId,
+    description:issueDesc,
+    severity:issueSeverity,
+    assignTo: issueAssignTo,
+    status:issuesSatus
+}
+    
+    if(localStorage.getItem['issues']==null){
+        var issues =[];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+        
+    }else{
+        var issues =JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+        
+    }
+    
+   // document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+        
+}
+
+
+
+function fetchIssues(){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+    
+    issuesList.innerHTML = '';
+        
+    for (var i=0; i<=issues.length; i++){
+        var id =issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignTo;
+        var status = issues[i].status;
+        
+        issuesList.innerHTML ='<div class="well">' +
+                               '<h6>Issues ID:' +
+                                '<p><span class"label label-info">' + status +'</span></p>'+
+                                '<h3>' + desc + '</h3>' +
+                                '<p><span class="glyphicon glyphicon-time"><span/>' + severity +'</span>' +
+                                '<p><span class="glyphicon glyphicon-user"><span/>' + assignedTo +'</span>'+
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning" >Close</a>'+
+                                '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-warning" >Do latter</a>'+
+                                '</div>';
+             
+
+    }
+    
+}
